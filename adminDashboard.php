343e1c891fec584e0aaf30d7fd2d0a53f64d71bd
O Prathyusha <oprathyusha23@gmail.com>
2020-12-24 20:28:05 +0530

    Update adminDashboard.php

--- adminDashboard.php
+++ adminDashboard.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Management System</title>
     <link rel="stylesheet" href="adminDashboard.css?v=<?php echo time(); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
