ed2fbd5adcf5afb5bc58ebc88135d11b86f91f93
O Prathyusha <oprathyusha23@gmail.com>
2021-01-11 22:22:37 +0530

    update

--- editStudent.php
+++ editStudent.php
@@ -12,14 +12,14 @@ if(mysqli_num_rows($query_run)>0)
 	 where USN='$_POST[USN]'";
 	$query_run = mysqli_query($connection,$query);
 
-	$query  = "update attendance set semester1=$_POST[semester1],semester2=$_POST[semester2],
-	semester3=$_POST[semester3],semester4=$_POST[semester4],semester5=$_POST[semester5],
-	semester6=$_POST[semester6],semester7=$_POST[semester7],semester8=$_POST[semester8] where USN='$_POST[USN]'";
+	$query  = "update attendance set semester1='$_POST[semester1]',semester2='$_POST[semester2]',
+	semester3='$_POST[semester3]',semester4='$_POST[semester4]',semester5='$_POST[semester5]',
+	semester6='$_POST[semester6]',semester7='$_POST[semester7]',semester8='$_POST[semester8]' where USN='$_POST[USN]'";
 	$query_run = mysqli_query($connection,$query);
 	   
-	$query  = "update result set sgpa1=$_POST[sgpa1],sgpa2=$_POST[sgpa2],
-	sgpa3=$_POST[sgpa3],sgpa4=$_POST[sgpa4],sgpa5=$_POST[sgpa5],
-	sgpa6=$_POST[sgpa6],sgpa7=$_POST[sgpa7],sgpa8=$_POST[sgpa8] where USN='$_POST[USN]'";
+	$query  = "update result set sgpa1='$_POST[sgpa1]',sgpa2='$_POST[sgpa2]',
+	sgpa3='$_POST[sgpa3]',sgpa4='$_POST[sgpa4]',sgpa5='$_POST[sgpa5]',
+	sgpa6='$_POST[sgpa6]',sgpa7='$_POST[sgpa7]',sgpa8='$_POST[sgpa8]' where USN='$_POST[USN]'";
 	$query_run = mysqli_query($connection,$query);
 	?>
 	<script type="text/javascript">
